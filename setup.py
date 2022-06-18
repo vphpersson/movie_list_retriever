559dd503103f9e5b6dac41b0bda7240aa300fef3
Viktor Persson <vph.persson@gmail.com>
2022-06-18 21:41:29 +0200

    Add missing comma in `setup.py`.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='movie_list_retriever',
-    version='0.11',
+    version='0.12',
     packages=find_packages(),
     install_requires=[
         'httpx',
-        'playwright'
+        'playwright',
         'typed_argument_parser @ git+https://github.com/vphpersson/typed_argument_parser.git#egg=typed_argument_parser'
     ]
 )
